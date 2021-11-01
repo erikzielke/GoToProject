48dd6a6fdcb0f4d7095f3361b56bd96ed739a82b
Erik Zielke <erikzielke@hotmail.com>
2021-11-01 11:42:13 +0100

    support for search everywhere

---
+++ src/main/kotlin/org/github/erikzielke/gotoproject/searcheverywhere/GoToProjectSearchEverywhereContributor.kt
@@ -0,0 +1,4 @@
+package org.github.erikzielke.gotoproject.searcheverywhere
+
+class GoToProjectSearchEverywhereContributor {
+}
---
+++ src/main/kotlin/org/github/erikzielke/gotoproject/searcheverywhere/GoToProjectSearchEverywhereContributorFactory.kt
@@ -0,0 +1,7 @@
+package org.github.erikzielke.gotoproject.searcheverywhere
+
+import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributorFactory
+
+class GoToProjectSearchEverywhereContributor: SearchEverywhereContributorFactory<Any> {
+
+}
