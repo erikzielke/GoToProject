b6b1b13d9a1d4ac375827844dbd9dfec733deaa1
Erik Zielke <erikzielke@hotmail.com>
2025-06-15 14:08:19 +0200

    chore: add placeholder test skeletons for GoToProject components

---
+++ src/test/kotlin/org/github/erikzielke/gotoproject/searcheverywhere/GoToProjectSearchEverywhereContributorFactoryTest.kt
@@ -0,0 +1,22 @@
+package org.github.erikzielke.gotoproject.searcheverywhere
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.mockito.kotlin.mock
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class GoToProjectSearchEverywhereContributorFactoryTest {
+    
+    @Test
+    fun `createContributor should return GoToProjectSearchEverywhereContributor`() {
+        // Arrange
+        val factory = GoToProjectSearchEverywhereContributorFactory()
+        val mockEvent = mock<AnActionEvent>()
+        
+        // Act
+        val contributor = factory.createContributor(mockEvent)
+        
+        // Assert
+        assertTrue(contributor is GoToProjectSearchEverywhereContributor)
+    }
+}
