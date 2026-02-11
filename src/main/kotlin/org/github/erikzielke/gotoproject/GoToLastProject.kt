e97a03d9860cd2381286dd34ddfd3081ebd8f399
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2026-02-11 02:10:33 -0800

    feat: Disable "Go to Last Project" in menu when no 'last project' was opened.

--- src/main/kotlin/org/github/erikzielke/gotoproject/GoToLastProject.kt
+++ src/main/kotlin/org/github/erikzielke/gotoproject/GoToLastProject.kt
@@ -1,16 +1,24 @@
 package org.github.erikzielke.gotoproject
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.wm.WindowManager
 import org.github.erikzielke.gotoproject.GoToProjectApplicationComponent.Companion.instance
 
 class GoToLastProject : AnAction() {
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
     override fun actionPerformed(anActionEvent: AnActionEvent) {
         val focusedBefore = instance.focusedBefore ?: return
         val manager = WindowManager.getInstance()
         val jFrame = manager.getFrame(focusedBefore)
         jFrame?.requestFocus()
+    }
+
+    override fun update(e: AnActionEvent) {
+        val shouldBeEnabled = instance.focusedBefore != null
+        e.presentation.isEnabled = shouldBeEnabled
     }
 
     override fun isDumbAware() = true
